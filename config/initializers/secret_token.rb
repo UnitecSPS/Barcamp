89011cc19af7f9c7e548b9f8794aef3cbb31f484
Carlos Gochez <cgochez@lawgical.com>
2012-05-11 00:11:21 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barcamp::Application.config.secret_token = '9a74bbf29bbf140a996984f6c55501c4671e394411c0474aff37a39681949bd06060cf7a162a33b5bd55142ddd6fc21d958a9ffabb807214fc9af7e94b3284b2'
